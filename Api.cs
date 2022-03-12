4970a678cfd9114fb696aeb162c555ef24058660
KK <morrkade05@gmail.com>
2022-03-11 20:08:04 -0600

    Initial mod

---
+++ Api.cs
@@ -0,0 +1,198 @@
+﻿using Platform.Steam;
+using System;
+using System.Xml;
+using System.Text;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+using _7DTDWebsockets.patchs;
+
+namespace _7DTDWebsockets
+{
+    public class API : IModApi
+    {
+        public void InitMod(Mod mod)
+        {
+            string path = ModManager.GetMod("WebsocketIntegration").Path + "/Config.xml";
+            
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.ConformanceLevel = ConformanceLevel.Fragment;
+            settings.IgnoreWhitespace = true;
+            settings.IgnoreComments = true;
+            XmlReader reader = XmlReader.Create(path, settings);
+
+            Dictionary<string, object> data = new Dictionary<string, object>();
+
+            Log.Out($"[Websocket] Attempting read of \"{path}\"");
+
+            while (reader.Read())
+            {
+                if (reader.IsStartElement())
+                {
+                    if (reader.IsEmptyElement) return;
+                    string n;
+                    string s;
+                    n = reader.Name;
+                    reader.Read();
+                    if (reader.IsStartElement()) n = reader.Name;
+                    s = reader.ReadString();
+                    data.Add(n, s);
+                }
+            }
+
+            foreach (var i in data)
+            {
+                Log.Out($"[Websocket] Config: {i.Key} : {i.Value}");
+            }
+
+            string host = (data["Host"] ?? "localhost").ToString();
+            string port = (data["Port"] ?? "9000").ToString();
+
+            RunTimePatch.PatchAll();
+            Websocket.Start(host + ":" + port);
+            ModEvents.GameShutdown.RegisterHandler(GameShutdown);
+            ModEvents.ChatMessage.RegisterHandler(ChatMessage);
+            ModEvents.PlayerLogin.RegisterHandler(PlayerLogin);
+            ModEvents.PlayerDisconnected.RegisterHandler(PlayerDisconnect);
+            ModEvents.PlayerSpawnedInWorld.RegisterHandler(PlayerSpawnedInWorld);
+        }
+
+        private void GameShutdown()
+        {
+            Websocket.Stop();
+        }
+
+
+        private class ChatMsg
+        {
+            public Player player;
+            public string Message;
+            public ChatMsg(Player pl, string msg)
+            {
+                player = pl;
+                Message = msg;
+            }
+        }
+        private bool ChatMessage(ClientInfo clientInfo, EChatType chatType, int senderId, string message, string mainName, bool localizeMain, List<int> recipientEntityIds)
+        {
+            if (clientInfo == null || string.IsNullOrEmpty(message)) return true;
+            ChatMsg m = new ChatMsg(new Player(clientInfo), message);
+            Send("ChatMessage", JsonConvert.SerializeObject(m));
+            return true;
+        }
+
+        private class PlayerOnlyObj
+        {
+            public Player player;
+            public PlayerOnlyObj(Player pl)
+            {
+                player = pl;
+            }
+        }
+
+        private bool PlayerLogin(ClientInfo clientInfo, string noIdea, StringBuilder stringBuilder)
+        {
+            if (clientInfo == null) return true;
+            Send("PlayerJoin", JsonConvert.SerializeObject(new PlayerOnlyObj(new Player(clientInfo))));
+            return true;
+        }
+
+        private void PlayerDisconnect(ClientInfo clientInfo, bool idk)
+        {
+            if (clientInfo == null) return;
+            Send("PlayerLeave", JsonConvert.SerializeObject(new PlayerOnlyObj(new Player(clientInfo))));
+        }
+
+        class PlayerSpawnIn
+        {
+            public Player player;
+            public string type;
+            public PlayerSpawnIn(Player player, string type)
+            {
+                this.player = player;
+                this.type = type;
+            }
+        }
+
+        private void PlayerSpawnedInWorld(ClientInfo clientInfo, RespawnType respawnType, Vector3i vector3I)
+        {
+            if (clientInfo == null) return;
+            Send("PlayerSpawnIn", JsonConvert.SerializeObject(new PlayerSpawnIn(new Player(clientInfo), respawnType.ToString())));
+        }
+
+        public class Player
+        {
+            public string name;
+
+            public Player (ClientInfo clientInfo)
+            {
+                this.name = clientInfo.playerName ?? "Unknwon";
+            }
+
+            public Player (EntityPlayer player)
+            {
+                this.name = player.EntityName;
+            }
+        }
+
+        public static void Send(string eventName, string arguments)
+        {
+            Websocket.Send(eventName + " " + arguments);
+        }
+
+        public class Websocket
+        {
+            public static WebSocketServer server;
+            public static EventSocket eventSocket;
+
+            public static void Start(string host)
+            {
+                Log.Out($"[Websocket] Starting socket server on: ws://{host}/");
+                server = new WebSocketServer("ws://" + host);
+                server.AddWebSocketService<EventSocket>("/");
+                server.Start();
+            }
+
+            public static void Stop()
+            {
+                if (server != null) server.Stop();
+            }
+
+            public static void Send(string message)
+            {
+                if (server == null || eventSocket == null) return;
+                eventSocket.SendBroadcast(message);
+            }
+
+            public class EventSocket : WebSocketBehavior
+            {
+                public void SendBroadcast(string msg)
+                {
+                    Sessions.Broadcast(msg);
+                }
+
+                protected override void OnOpen()
+                {
+                    if (eventSocket == null) eventSocket = this;
+                }
+
+                protected override void OnError(ErrorEventArgs e)
+                {
+                    if (eventSocket == null) eventSocket = this;
+                }
+
+                protected override void OnClose(CloseEventArgs e)
+                {
+                    if (eventSocket == null) eventSocket = this;
+                }
+
+                protected override void OnMessage(MessageEventArgs e)
+                {
+                    Sessions.Broadcast(e.Data);
+                }
+            }
+        }
+    }
+}
---
+++ Harmony/RunTimePatch.cs
@@ -0,0 +1,174 @@
+﻿using HarmonyLib;
+using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using GUI_2;
+using UnityEngine;
+
+namespace _7DTDWebsockets.patchs
+{
+    internal class RunTimePatch
+    {
+        public static void PatchAll()
+        {
+            Log.Out("[Websocket] Runtime patches intinialized");
+            Harmony harmony = new Harmony("com.gmail.kk964gaming.websockets.patch");
+            Harmony.DEBUG = true;
+            harmony.PatchAll();
+
+            foreach (var method in harmony.GetPatchedMethods())
+            {
+                Log.Out($"Successfully patched: {method.Name}");
+            }
+        }
+    }
+}
+
+public class Player
+{
+    public string name;
+
+    public Player(ClientInfo clientInfo)
+    {
+        this.name = clientInfo.playerName ?? "Unknwon";
+    }
+
+    public Player(EntityPlayer player)
+    {
+        this.name = player.EntityName;
+    }
+}
+
+class PlayerDmgEvent
+{
+    public Player player;
+    public string cause;
+    public int damage;
+    public PlayerDmgEvent(Player player, string cause, int damage)
+    {
+        this.player = player;
+        this.cause = cause;
+        this.damage = damage;
+    }
+}
+
+[HarmonyPatch]
+class ClientPlayerDamagePatch
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(EntityAlive), "DamageEntity")]
+    static void Prefix(EntityAlive __instance, DamageSource _damageSource, int _strength, bool _criticalHit, float _impulseScale = 1f)
+    {
+        Log.Out($"EntityAlive damage : {__instance.GetDebugName()} {_strength}");
+        if (!(__instance is EntityPlayer)) return;
+        EntityPlayer player = (EntityPlayer)__instance;
+        _7DTDWebsockets.API.Send("PlayerDamage", JsonConvert.SerializeObject(new PlayerDmgEvent(new Player(player), _damageSource.damageType.ToString(), _strength)));
+        return;
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(Entity), "DamageEntity")]
+    static void Prefix2(Entity __instance, DamageSource _damageSource, int _strength, bool _criticalHit, float impulseScale = 1f)
+    {
+        Log.Out($"Entity damage : {__instance.GetDebugName()} {_strength}");
+        if (!(__instance is EntityPlayer)) return;
+        EntityPlayer player = (EntityPlayer)__instance;
+        _7DTDWebsockets.API.Send("PlayerDamage", JsonConvert.SerializeObject(new PlayerDmgEvent(new Player(player), _damageSource.damageType.ToString(), _strength)));
+        return;
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(EntityPlayer), "DamageEntity")]
+    static void Prefix3(EntityPlayer __instance, DamageSource _damageSource, int _strength, bool _criticalHit, float _impulseScale = 1f)
+    {
+        Log.Out($"EntityAlive damage : {__instance.GetDebugName()} {_strength}");
+        EntityPlayer player = __instance;
+        _7DTDWebsockets.API.Send("PlayerDamage", JsonConvert.SerializeObject(new PlayerDmgEvent(new Player(player), _damageSource.damageType.ToString(), _strength)));
+        return;
+    }
+}
+
+class PlayerKillEntityEvent
+{
+    public Player player;
+    public string entity;
+    public bool animal;
+    public bool zombie;
+    public PlayerKillEntityEvent(Player player, string entity, bool animal, bool zombie)
+    {
+        this.player = player;
+        this.entity = entity;
+        this.animal = animal;
+        this.zombie = zombie;
+    }
+}
+
+class PlayerEntityEvent
+{
+    public Player player;
+    public string entity;
+    public PlayerEntityEvent(Player player, string entity)
+    {
+        this.player = player;
+        this.entity = entity;
+    }
+}
+
+class PlayerOnlyEvent
+{
+    public Player player;
+    public PlayerOnlyEvent(Player player)
+    {
+        this.player = player;
+    }
+}
+
+class PlayerDeathEvent
+{
+    public Player player;
+    public PlayerDeathEvent(Player player)
+    {
+        this.player = player;
+    }
+}
+
+[HarmonyPatch(typeof(EntityAlive), "SetDead")]
+class PatchEntityDeath
+{
+    static bool Prefix(EntityAlive __instance)
+    {
+        object obj = Traverse.Create(__instance).Field("entityThatKilledMe").GetValue();
+        if (__instance is EntityPlayer) return true;
+        if (obj == null) return true;
+        EntityAlive whokilledMe = (EntityAlive)obj;
+        if (whokilledMe == null) return true;
+        if (!(whokilledMe is EntityPlayer)) return true;
+        EntityPlayer player = whokilledMe as EntityPlayer;
+        string ent = __instance.GetDebugName();
+
+        bool animal = false;
+        bool zombie = false;
+
+        if (ent.ToLower().Contains("animal")) animal = true;
+        if (ent.ToLower().Contains("zombie")) zombie = true;
+
+        if (animal) ent = Regex.Replace(ent, "(animal)", "", RegexOptions.IgnoreCase);
+        if (zombie) ent = Regex.Replace(ent, "(zombie)", "", RegexOptions.IgnoreCase);
+
+        _7DTDWebsockets.API.Send("PlayerKillEntity", JsonConvert.SerializeObject(new PlayerKillEntityEvent(new Player(player), ent, animal, zombie)));
+
+        if (animal && !zombie)
+        {
+            _7DTDWebsockets.API.Send("PlayerKillAnimal", JsonConvert.SerializeObject(new PlayerEntityEvent(new Player(player), ent)));
+        }
+
+        if (zombie)
+        {
+            _7DTDWebsockets.API.Send("PlayerKillZombie", JsonConvert.SerializeObject(new PlayerEntityEvent(new Player(player), ent)));
+        }
+
+        return true;
+    }
+}
+
